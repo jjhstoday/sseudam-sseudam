af571856479b5ad43a3e275b9e6516e7dfeb8cc2
jjhstoday <jjh.s.today@gmail.com>
2021-09-28 00:38:21 +0900

    feat: Home & Posting & Book & Sentence 컴포넌트 라우터 적용

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from 'pages/Home/Home';
+import Book from 'pages/Book/Book';
+import PostingBook from 'pages/PostingBook/PostingBook';
+import PostingSentence from 'pages/PostingSentence/PostingSentence';
+import Sentence from 'pages/Sentence/Sentence';
+import GlobalStyle from 'styles';
 
 function App() {
-  return <div className='App'>hi</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/book/:bookId' component={Book} />
+        <Route path='/postingBook' component={PostingBook} />
+        <Route path='/book/:bookId/sentence/:sentenceId' component={Sentence} />
+        <Route
+          path='/book/:bookId/sentence/postingSentence'
+          component={PostingSentence}
+        />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
