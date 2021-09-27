64b7f4c1fd7536100d7cf29501df60e99cd17a61
jjhstoday <jjh.s.today@gmail.com>
2021-09-28 00:41:33 +0900

    style: light & dark theme 설정

---
+++ client/src/styles/color.ts
@@ -0,0 +1,13 @@
+export const lightPalette = {
+  themePrimary: '#F7F5F3',
+  themeSecondary: '#74592F',
+  themePrimaryFont: '#7E6227',
+  themeSecondaryFont: '#404040'
+};
+
+export const darkPalette = {
+  themePrimary: '#282B36',
+  themeSecondary: '#9EAAD1',
+  themePrimaryFont: '#9EAAD1',
+  themeSecondaryFont: '#E1E4EB'
+};
