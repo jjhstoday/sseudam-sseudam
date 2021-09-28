566948916ec896ad0d214d3efd33fe8d5e385755
jjhstoday <jjh.s.today@gmail.com>
2021-09-29 02:28:44 +0900

    fix: 페이지 이동시 exact를 넣어 경로 정확 매치

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from 'pages/Home/Home';
-import Book from 'pages/Book/Book';
-import PostingBook from 'pages/PostingBook/PostingBook';
-import PostingSentence from 'pages/PostingSentence/PostingSentence';
-import Sentence from 'pages/Sentence/Sentence';
+import Home from 'pages/Home';
+import Book from 'pages/Book';
+import PostingBook from 'pages/PostingBook';
+import PostingSentence from 'pages/PostingSentence';
+import Sentence from 'pages/Sentence';
 import GlobalStyle from 'styles';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <>
       <GlobalStyle />
       <Switch>
-        <Route path='/' component={Home} />
+        <Route exact path='/' component={Home} />
         <Route path='/book/:bookId' component={Book} />
         <Route path='/postingBook' component={PostingBook} />
         <Route path='/book/:bookId/sentence/:sentenceId' component={Sentence} />
