500eb8d07126863d33b61484dab0153dffadc3cc
jjhstoday <jjh.s.today@gmail.com>
2021-09-30 17:50:48 +0900

    feat: 홈에서 포스팅 페이지로 이동 시 스크롤 위치 값 맨 위로 설정

--- client/src/pages/PostingBook/index.tsx
+++ client/src/pages/PostingBook/index.tsx
@@ -1,13 +1,17 @@
 import Footer from 'components/Footer';
-import Header from 'components/Header';
 import SearchingBookContainer from 'containers/SearchingBookContainer';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function PostingBook() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
-      <Header title='책 등록' />
-      <SearchingBookContainer />
+      <section>
+        <SearchingBookContainer />
+      </section>
       <Footer />
     </>
   );
