ec4102d398e064f530f0f0f8ed92c5af35f84093
jjhstoday <jjh.s.today@gmail.com>
2021-10-01 01:41:19 +0900

    fix: history.goback에 따라 생기는 오류 수정

--- client/src/components/CloseButton/index.tsx
+++ client/src/components/CloseButton/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { CloseIcon, Button } from './styles';
 
 export default function CloseButton() {
   const history = useHistory();
+  const location = useLocation();
+  const { pathname } = location;
 
   const onClick = () => {
-    history.goBack();
+    if (pathname.includes('book') && !pathname.includes('postingSentence')) {
+      history.push('/');
+    } else history.goBack();
   };
 
   return (
