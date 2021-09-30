8eb2f2bb178797aed9a2683f173db81790cd7cf9
jjhstoday <jjh.s.today@gmail.com>
2021-09-30 22:47:27 +0900

    style: Book 리스트 책장 스타일로 스타일 변경

--- client/src/components/Book/styles.tsx
+++ client/src/components/Book/styles.tsx
@@ -1,14 +1,34 @@
 import styled from 'styled-components';
 
 export const Li = styled.li`
-  width: 45%;
-  height: 230px;
-  box-shadow: 2px 2px 5px gray;
+  width: 25%;
+  height: 120px;
+  margin: 0 1rem;
   margin-bottom: 7rem;
-  overflow: hidden;
+  display: flex;
+  justify-content: center;
+
+  &:nth-child(3n + 1) {
+    position: relative;
+
+    &::after {
+      content: '';
+      width: 375%;
+      height: 2px;
+      display: block;
+      position: absolute;
+      bottom: -0.2rem;
+      left: 1px;
+      z-index: 1;
+      background: gray;
+      box-shadow: 0px 3px 5px black;
+      opacity: 0.7;
+    }
+  }
 
   img {
-    width: 100%;
+    width: 80px;
     height: 100%;
+    box-shadow: 2px 2px 5px gray;
   }
 `;
