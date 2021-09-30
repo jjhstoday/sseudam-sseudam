114359b8b80ba5f0ded4d8256f8ed434e9f6cfca
jjhstoday <jjh.s.today@gmail.com>
2021-10-01 00:29:31 +0900

    style: 책 상세 페이지에서 책 제목 줄바꿈 깨지는 현상 해결

--- client/src/components/Header/styles.tsx
+++ client/src/components/Header/styles.tsx
@@ -13,10 +13,18 @@ export const HeaderContainer = styled.header`
   z-index: 3;
 
   h2 {
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    justify-content: center;
     text-align: center;
-    line-height: 135px;
+    height: 135px;
+    line-height: 4rem;
     font-size: 2.5rem;
     font-family: designhouseOTFLight00;
+    width: 55vw;
+    margin: 0 auto;
+    word-break: keep-all;
   }
 
   ${Divider} {
