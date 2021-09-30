fbb6a27ee533ba07edffa3a0d9808983f94123e5
jjhstoday <jjh.s.today@gmail.com>
2021-10-01 01:13:19 +0900

    style: 책 / 구절 등록에 따른 버튼 색상 변경

--- client/src/components/AddButton/index.tsx
+++ client/src/components/AddButton/index.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { IconContainer, PlusIcon } from './styles';
+import { lightPalette } from 'styles';
 
 interface Props {
   moveToPage: string;
+  id?: string;
 }
+const AddButton: FC<Props> = ({ moveToPage, id = '' }) => {
+  const color =
+    moveToPage === '/postingBook'
+      ? lightPalette.themeSecondaryFont
+      : lightPalette.themePrimaryFont;
 
-const AddButton: FC<Props> = ({ moveToPage }) => {
   return (
-    <Link to='/postingBook'>
-      <IconContainer>
+    <Link
+      to={{
+        pathname: moveToPage,
+        state: {
+          id
+        }
+      }}
+    >
+      <IconContainer $color={color}>
         <PlusIcon />
       </IconContainer>
     </Link>
--- client/src/components/AddButton/styles.tsx
+++ client/src/components/AddButton/styles.tsx
@@ -2,15 +2,17 @@ import styled from 'styled-components';
 import { GiFeather } from 'react-icons/gi';
 import { lightPalette } from 'styles';
 
+interface Props {
+  $color: string;
+}
 export const PlusIcon = styled(GiFeather)``;
-
-export const IconContainer = styled.div`
+export const IconContainer = styled.div<Props>`
   width: 60px;
   height: 60px;
   border-radius: 50%;
   padding: 17px;
-  box-shadow: 0px 0px 8px ${lightPalette.themeSecondaryFont};
-  background: ${lightPalette.themeSecondaryFont};
+  box-shadow: 0px 0px 8px ${props => props.$color};
+  background: ${props => props.$color};
   position: fixed;
   z-index: 1;
   bottom: 7%;
