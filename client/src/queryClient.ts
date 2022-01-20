fb8d47ec166ffd99e3b461ea602c96fd2dd2254f
jihye <jjh@lodics.com>
2022-01-20 11:06:38 +0900

    build: aws ip 적용

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -2,15 +2,14 @@ import { request } from 'graphql-request';
 import { DocumentNode } from 'graphql/language/ast';
 const URL =
   process.env.NODE_ENV === 'production'
-    ? 'http://52.14.101.121:8000/graphql'
+    ? `${process.env.REACT_APP_SERVER_HOST}:8000/graphql`
     : 'http://localhost:8000/graphql';
 
-export const fetcher = (query: DocumentNode, variables = {}) =>
-  request(URL, query, variables);
+export const fetcher = (query: DocumentNode, variables = {}) => request(URL, query, variables);
 
 export const QueryKeys = {
   BOOKS: 'BOOKS',
   BOOK: 'BOOK',
   SENTENCES: 'SENTENCES',
-  SENTENCE: 'SENTENCE'
+  SENTENCE: 'SENTENCE',
 };
